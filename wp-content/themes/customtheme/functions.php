a5eda6dcff99087cc077e261deb03e8e95005c43
Micha <micha-elmans@outlook.com>
2024-11-27 12:01:14 +0100

    added basic stuff for a plugin

---
+++ wp-content/themes/customtheme/functions.php
@@ -0,0 +1,15 @@
+<?php
+function my_custom_theme_setup() {
+    // Add theme support
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    register_nav_menus([
+        'main-menu' => 'Main Menu',
+    ]);
+}
+add_action('after_setup_theme', 'my_custom_theme_setup');
+
+function my_custom_theme_enqueue_styles() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue_styles');
