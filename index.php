c6e64b466420864999a41d011e0020dd06af619a
Micha <micha-elmans@outlook.com>
2024-12-04 12:00:01 +0100

    made it so the git is now only the theme

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+    <?php wp_head(); ?>
+</head>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+<body <?php body_class(); ?>>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<header>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+    <nav class="header-nav" ><?php wp_nav_menu(['theme_location' => 'main-menu']); ?></nav>
+</header>
+
+<main>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div><?php the_content(); ?></div>
+        </article>
+    <?php endwhile; else : ?>
+        <p>No content found.</p>
+    <?php endif; ?>
+</main>
+
+<footer>
+    <p> &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
