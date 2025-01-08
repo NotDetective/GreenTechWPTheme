04a5fe8752e71ee7e157c2dd401f78f7fe0bc6ab
Micha <micha-elmans@outlook.com>
2025-01-08 11:18:44 +0100

    added all 3 post loops

--- index.php
+++ index.php
@@ -15,60 +15,123 @@
     <nav class="header-nav"><?php wp_nav_menu(['theme_location' => 'main-menu']); ?></nav>
 </header>
 
-<div id="main">
+<!-- Wrapper -->
+<div id="wrapper">
 
-    <!-- Post -->
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div id="main">
 
-        <article class="post">
-            <header>
-                <div class="title">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <p>
-                        <?php
+        <!-- Post -->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                        ?>
-                    </p>
-                </div>
-                <div class="meta">
-                    <time class="published" datetime="2015-11-01">
-                        <?php the_time('F j, Y'); ?>
-                    </time>
-                    <a href="#" class="author"><span class="name">
+            <article class="post">
+                <header>
+                    <div class="title">
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <p>
+                            <?php
+
+                            ?>
+                        </p>
+                    </div>
+                    <div class="meta">
+                        <time class="published" datetime="2015-11-01">
+                            <?php the_time('F j, Y'); ?>
+                        </time>
+                        <a href="user/post/<?php echo the_author_meta('ID'); ?>" class="author"><span class="name">
                         <?php the_author(); ?>
                         </span><img src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>"
                                     alt=""/></a>
-                </div>
+                    </div>
+                </header>
+                <a href="<?php the_permalink(); ?>" class="image featured"><img
+                            src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""/></a>
+                <p>
+                    <?php the_excerpt(); ?>
+                </p>
+                <footer>
+                    <ul class="actions">
+                        <li><a href="<?php the_permalink(); ?>" class="button large">Continue Reading</a></li>
+                    </ul>
+                    <ul class="stats">
+                        <li><a href="#">General</a></li>
+                        <li><a href="#" class="icon solid fa-comment">
+                                <?php echo get_comments_number(); ?>
+                            </a></li>
+                    </ul>
+                </footer>
+            </article>
+
+        <?php endwhile; else : ?>
+            <p>No content found.</p>
+        <?php endif; ?>
+
+        <!-- Pagination -->
+        <ul class="actions pagination">
+            <li><a href="<?php echo get_previous_posts_page_link(); ?>"
+                   class="<?php echo is_front_page() ? 'button large disabled' : 'button large'; ?>"
+                >Previous Page</a></li>
+            <li><a href="<?php echo get_next_posts_page_link(); ?>" class="button large next">Next Page</a></li>
+        </ul>
+
+    </div>
+
+    <section id="sidebar">
+
+        <!-- Intro -->
+        <section id="intro">
+            <header>
+                <h2>
+                    <?php bloginfo('name'); ?>
+                </h2>
+                <p>
+                    <?php bloginfo('description'); ?>
+                </p>
             </header>
-            <a href="<?php the_permalink(); ?>" class="image featured"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""/></a>
-            <p>
-                <?php the_excerpt(); ?>
-            </p>
-            <footer>
-                <ul class="actions">
-                    <li><a href="<?php the_permalink(); ?>" class="button large">Continue Reading</a></li>
-                </ul>
-                <ul class="stats">
-                    <li><a href="#">General</a></li>
-                    <li><a href="#" class="icon solid fa-heart">28</a></li>
-                    <li><a href="#" class="icon solid fa-comment">
-                            <?php echo get_comments_number(); ?>
-                        </a></li>
-                </ul>
-            </footer>
-        </article>
+        </section>
 
-    <?php endwhile; else : ?>
-        <p>No content found.</p>
-    <?php endif; ?>
+        <!-- Mini Posts -->
+        <section>
+            <div class="mini-posts">
 
-    <!-- Pagination -->
-    <ul class="actions pagination">
-        <li><a href="<?php echo get_previous_posts_page_link(); ?>"
-               class="<?php echo is_front_page() ? 'button large disabled' : 'button large'; ?>"
-            >Previous Page</a></li>
-        <li><a href="<?php echo get_next_posts_page_link(); ?>" class="button large next">Next Page</a></li>
-    </ul>
+                <!-- Mini Post -->
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article class="mini-post">
+                        <header>
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <time class="published" datetime="2015-10-20"><?php the_time('F j, Y'); ?></time>
+                            <a href="user/post/<?php echo the_author_meta('ID'); ?>"
+                               class="author">
+                                <img src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>" alt=""/>
+                            </a>
+                        </header>
+                        <a href="<?php the_permalink(); ?>" class="image"><img
+                                    src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""/></a>
+                    </article>
+                <?php endwhile; else : ?>
+                    <p>No content found.</p>
+                <?php endif; ?>
+            </div>
+        </section>
+
+        <section>
+            <ul class="posts">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <li>
+                        <article>
+                            <header>
+                                <h3><a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a></h3>
+                                <time class="published" datetime="2015-10-20"><?php the_time('F j, Y'); ?></time>
+                            </header>
+                            <a href="<?php the_permalink(); ?>" class="image"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""/></a>
+                        </article>
+                    </li>
+                <?php endwhile; else : ?>
+                    <p>No content found.</p>
+                <?php endif; ?>
+            </ul>
+        </section>
 
 </div>
 
