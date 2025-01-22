6c628eaddb0e9d6ec6aab6ab380162b940553552
bleenie <boleenders01@gmail.com>
2025-01-22 12:05:31 +0100

    added js scripts to functions.php

--- functions.php
+++ functions.php
@@ -17,3 +17,21 @@ function my_custom_theme_enqueue_styles()
 
 add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue_styles');
 
+function enqueue_theme_scripts()
+{
+    // Enqueue jquery.min.js
+    wp_enqueue_script('jquery-script', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.0.0', true);
+
+    // Enqueue the breakpoint.min.js script
+    wp_enqueue_script('breakpoint-script', get_template_directory_uri() . '/js/breakpoint.min.js', array(), '1.0.0', true);
+
+    // Enqueue browser.min.js
+    wp_enqueue_script('browser-script', get_template_directory_uri() . '/js/browser.min.js', array(), '1.0.0', true);
+
+    // Enqueue main.js
+    wp_enqueue_script('main-script', get_template_directory_uri() . '/js/main.js', array('jquery-script'), '1.0.0', true);
+
+    // Enqueue util.js
+    wp_enqueue_script('util-script', get_template_directory_uri() . '/js/util.js', array('jquery-script'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_theme_scripts');
