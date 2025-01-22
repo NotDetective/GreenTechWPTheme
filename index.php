9adc86795dfdeb9c45a925abc704cd380c616993
bleenie <boleenders01@gmail.com>
2025-01-22 12:53:25 +0100

    added sidebar

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,88 +10,90 @@
 
 <body <?php body_class(); ?>>
 
-<?php require 'parts/header.php'; ?>
+    <div id="wrapper">
+        <?php require 'parts/header.php'; ?>
 
-    <div id="main">
+        <div id="main">
 
-        <!-- Post -->
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <!-- Post -->
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post(); ?>
 
-            <article class="post">
-                <header>
-                    <div class="title">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <p>
-                            <?php
-                            the_category(', ');
-                            ?>
-                        </p>
-                    </div>
-                    <div class="meta">
-                        <time class="published" datetime="2015-11-01">
-                            <?php the_time('F j, Y'); ?>
-                        </time>
-                        <a href="user/post/<?php echo the_author_meta('ID'); ?>" class="author">
-                            <span class="name">
-                                <?php the_author(); ?>
-                            </span>
-                            <img src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>" alt=""/>
+                    <article class="post">
+                        <header>
+                            <div class="title">
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                <p>
+                                    <?php
+                                    the_category(', ');
+                                    ?>
+                                </p>
+                            </div>
+                            <div class="meta">
+                                <time class="published" datetime="2015-11-01">
+                                    <?php the_time('F j, Y'); ?>
+                                </time>
+                                <a href="user/post/<?php echo the_author_meta('ID'); ?>" class="author">
+                                    <span class="name">
+                                        <?php the_author(); ?>
+                                    </span>
+                                    <img src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>" alt="" />
+                                </a>
+
+                            </div>
+                        </header>
+                        <a href="<?php the_permalink(); ?>" class="image featured">
+                            <?php echo the_post_thumbnail('single-post-thumbnail'); ?>
                         </a>
+                        <p>
+                            <?php if (is_front_page()): ?>
+                                <?php the_excerpt(); ?>
+                            <?php elseif (!is_front_page()): ?>
+                                <?php the_content(); ?>
+                            <?php endif; ?>
+                        </p>
+                        <footer>
+                            <?php if (is_front_page()): ?>
+                                <ul class="actions">
+                                    <li><a href="<?php the_permalink(); ?>" class="button large">Continue Reading</a></li>
+                                </ul>
+                            <?php endif; ?>
+                            <ul class="stats">
+                                <li><a href="#">General</a></li>
+                                <li><a href="#" class="icon solid fa-comment">
+                                        <?php echo get_comments_number(); ?>
+                                    </a></li>
+                            </ul>
+                        </footer>
+                    </article>
 
-                    </div>
-                </header>
-                <a href="<?php the_permalink(); ?>" class="image featured">
-                    <?php echo the_post_thumbnail('single-post-thumbnail'); ?>
-                </a>
-                <p>
-                    <?php if (is_front_page()) : ?>
-                        <?php the_excerpt(); ?>
-                    <?php elseif (!is_front_page()) : ?>
-                        <?php the_content(); ?>
-                    <?php endif; ?>
-                </p>
-                <footer>
-                    <?php if (is_front_page()): ?>
-                        <ul class="actions">
-                            <li><a href="<?php the_permalink(); ?>" class="button large">Continue Reading</a></li>
-                        </ul>
-                    <?php endif; ?>
-                    <ul class="stats">
-                        <li><a href="#">General</a></li>
-                        <li><a href="#" class="icon solid fa-comment">
-                                <?php echo get_comments_number(); ?>
-                            </a></li>
-                    </ul>
-                </footer>
-            </article>
+                <?php endwhile; else: ?>
+                <p>No content found.</p>
+            <?php endif; ?>
 
-        <?php endwhile; else : ?>
-            <p>No content found.</p>
-        <?php endif; ?>
-
-        <!-- Pagination -->
-        <?php if (is_front_page()) : ?>
-            <ul class="actions pagination">
-                <li>
-                    <?php if (get_previous_posts_link()) : ?>
-                        <a href="<?php echo get_previous_posts_page_link(); ?>" class="button large">Previous Page</a>
-                    <?php else : ?>
-                        <a href="#" class="button large disabled">Previous Page</a>
-                    <?php endif; ?>
-                </li>
-                <li>
-                    <?php if (get_next_posts_link()) : ?>
-                        <a href="<?php echo get_next_posts_page_link(); ?>" class="button large next">Next Page</a>
-                    <?php else : ?>
-                        <a href="#" class="button large disabled">Next Page</a>
-                    <?php endif; ?>
-                </li>
-            </ul>
-        <?php endif; ?>
+            <!-- Pagination -->
+            <?php if (is_front_page()): ?>
+                <ul class="actions pagination">
+                    <li>
+                        <?php if (get_previous_posts_link()): ?>
+                            <a href="<?php echo get_previous_posts_page_link(); ?>" class="button large">Previous Page</a>
+                        <?php else: ?>
+                            <a href="#" class="button large disabled">Previous Page</a>
+                        <?php endif; ?>
+                    </li>
+                    <li>
+                        <?php if (get_next_posts_link()): ?>
+                            <a href="<?php echo get_next_posts_page_link(); ?>" class="button large next">Next Page</a>
+                        <?php else: ?>
+                            <a href="#" class="button large disabled">Next Page</a>
+                        <?php endif; ?>
+                    </li>
+                </ul>
+            <?php endif; ?>
+        </div>
+        <?php require 'parts/sidebar.php'; ?>
     </div>
-<footer>
-    <p> &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-</footer>
 
     <?php wp_footer(); ?>
 </body>
